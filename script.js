4298acf6f492a0c24012d0b574ae54b7baede002
Nik Pruss <nikpruss@gmail.com>
2021-04-03 21:26:51 -0500

    final

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {            
+            const div = document.getElementById("container");
+            for (let i=0; i<json.length; i++){                
+                div.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li id="active${i}">Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>                        
+                        <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+                console.log(document.getElementById("active"+i));
+                if (json[i].active) {
+                    document.getElementById("active"+i).style.color="green";
+                }
+            }
+        });
+    });
+});
